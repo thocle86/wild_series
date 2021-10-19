a04e44531764acbe0dae34768255257a16d972dc
thocle86 <thocle86@gmail.com>
2021-10-19 20:38:13 +0200

    Création de la liste des catégories + création du formulaire d'ajout et de modification et de suppression + ajustement du style

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required' => false,
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Catégorie',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+                'help' => 'Le nom de la catégorie doit faire entre 2 et 255 caractères',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
